5cca1fe5d3392ab7946960f09ea4894d05d779d5
shungo0222 <kimura.shungo@gmail.com>
2024-07-15 12:51:48 +0900

    Update Homepage Design: Implement new layout with adjustable background colors and improved component structure
    Related to #1

---
+++ src/app/constants/homepageData.ts
@@ -0,0 +1,114 @@
+export const navLinks = [
+  {
+    id: "#features",
+    label: "Features",
+  },
+  {
+    id: "#voices",
+    label: "Voices",
+  },
+  {
+    id: "#aboutus",
+    label: "About Us",
+  },
+  {
+    id: "#contactus",
+    label: "Contact Us",
+  },
+  {
+    id: "#faq",
+    label: "FAQ",
+  },
+];
+
+export const featuresForAdvertiser = [
+  {
+    title: "①TonとEVMのエコシステムを繋ぐ",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+  {
+    title: "②CPAやCPMを安価で設定可能",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+  {
+    title: "③TonやUSDT以外の通貨での支払いも受け付け",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+];
+
+export const featuresForApp = [
+  {
+    title: "①TonとEVMのエコシステムを繋ぐ",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+  {
+    title: "②CPAやCPMを安価で設定可能",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+  {
+    title: "③TonやUSDT以外の通貨での支払いも受け付け",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+];
+
+export const logos = [
+  "/buddy-arena.png",
+  "/world-of-bezogia.png",
+  "/farcana.png",
+  "/starheroes.png",
+  "/gumi.png",
+];
+
+export const faqs = [
+  {
+    question: "How does your affiliate vetting process work?",
+    answer: "We have a rigorous vetting process to ensure we partner with high-quality, reputable affiliates across Asia. Each applicant undergoes thorough evaluation based on their audience demographics, content quality, past performance metrics, and adherence to our guidelines. Only top-tier affiliates with proven track records are approved.",
+  },
+  {
+    question: "Can I run customized affiliate campaigns?",
+    answer: "Absolutely. Our platform offers extensive customization options, allowing you to tailor affiliate campaigns based on specific goals, target audiences, reward structures, and more. You have full control over campaign parameters, creative assets, and tracking mechanisms to ensure optimal performance.",
+  },
+  {
+    question: "What kind of support do you provide?",
+    answer: "We pride ourselves on providing exceptional customer support. Our dedicated account managers are available to assist you throughout the entire process, from campaign setup to optimization and payout management. We also offer comprehensive onboarding resources, knowledge base articles, and timely technical support.",
+  },
+  {
+    question: "How are affiliate payouts handled?",
+    answer: "Our platform supports secure, transparent payouts in both fiat currencies and cryptocurrencies. Affiliates can choose their preferred payout method, and all transactions are recorded on the blockchain for enhanced transparency and auditability. Payouts are processed on a pre-defined schedule, ensuring timely compensation for your affiliate partners.",
+  },
+];
+
+export const socialMediaLinks = [
+  { 
+    src: "/telegram.png", 
+    alt: "Telegram",
+    url: "https://telegram.org", 
+  },
+];
+
+export const footerLinks = {
+  "Company": [
+    {
+      url: "#",
+      label: "Home",
+    },
+    // {
+    //   url: "https://qube2-succery.vercel.app/corporate",
+    //   label: "About",
+    // },
+    {
+      url: "mailto:official@ceed.cloud",
+      label: "Contract",
+    },
+  ],
+  "Legal": [
+    {
+      url: "https://veroo.notion.site/Terms-and-Conditions-e56f60533a834abbbd1213c6bf0cd36f",
+      label: "Terms and Conditions",
+    },
+    {
+      url: "https://veroo.notion.site/Privacy-Policy-4538184bcaee4835a2dedcc464496cdd",
+      label: "Privacy Policy",
+    },
+  ],
+};
